f9e3d42e6c7064c9413f684a3037ec34777b2c3d
sagardubey47 <sagardubey47@gmail.com>
2021-04-15 12:59:49 +0530

    add structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-
+import React from "react"
+import "./app.css"
 
 function App() {
   return (
-    <div >
-       hello
+    <div className="app">
+      <header className="heading">
+         TODO APP (not yet connected to backend..)
+      </header>
+      <main className="main_body">
+         <section className="col-1">
+            column 1
+         </section>
+         <section className="col-2">
+            column 2
+         </section>
+         <section className="col-3">
+             column 3  
+         </section>
+      </main>
+      
     </div>
   );
 }
